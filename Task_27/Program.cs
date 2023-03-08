3d70d3eae67bf3cd62e31f81d8ebec13c95f0239
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-08 14:31:46 +0300

    Задание 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int num = GetNumberFromUser("Введите число: ", "Ошибка ввода");
+int sum = GetSumOfDigits(num);
+Console.WriteLine($"Сумма цифр числа {num} равна {sum}");
+
+int GetSumOfDigits(int orig)
+{
+    int summa = 0;
+    while (orig > 0)
+    {
+        summa += orig % 10;
+        orig = orig / 10;
+    }
+    return summa;
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
